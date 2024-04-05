7cfa4d3f85ab255dd150062c3a220b3dc4e60cad
Stefan Lucian Gramada <stefangramada09@gmail.com>
2024-04-05 16:26:36 +0300

    Implement getSizeList(), getMedicine(), modifyMedicine()

--- repository_medicine.cpp
+++ repository_medicine.cpp
@@ -4,6 +4,14 @@ vector<Medicine> MedicineRepository::getList() {
     return list;
 }
 
+size_t MedicineRepository::getSizeList() {
+    return list.size();
+}
+
+Medicine MedicineRepository::getMedicine(size_t index) {
+    return list[index];
+}
+
 void MedicineRepository::addMedicine(const Medicine &medicine) {
     list.push_back(medicine);
 }
@@ -13,5 +21,14 @@ void MedicineRepository::removeMedicine(const Medicine &medicine) {
 
     if(index != list.end()) {
         list.erase(index);
+    }
+}
+
+void MedicineRepository::modifyMedicine(const Medicine &old_medicine, const Medicine &new_medicine) {
+    for(Medicine & medicine:list) {
+        if(medicine == old_medicine) {
+            medicine = new_medicine;
+            return;
+        }
     }
 }
--- repository_medicine.h
+++ repository_medicine.h
@@ -13,9 +13,12 @@ private:
     vector<Medicine> list;
 public:
     vector<Medicine> getList();
+    size_t getSizeList();
+    Medicine getMedicine(size_t index);
 
     void addMedicine(const Medicine & medicine);
     void removeMedicine(const Medicine & medicine);
+    void modifyMedicine(const Medicine & old_medicine, const Medicine & new_medicine);
 };
 
 #endif //FARMACIE_REPOSITORY_MEDICINE_H
